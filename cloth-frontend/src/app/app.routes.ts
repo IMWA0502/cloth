406eadbbb0ccbea6768c0dc1e4655555517a7402
IMRAN WAHEED <imran.waheed@edu.unife.it>
2025-10-24 12:34:22 +0200

    cloth2

--- cloth-frontend/src/app/app.routes.ts
+++ cloth-frontend/src/app/app.routes.ts
@@ -4,11 +4,9 @@ import { About } from './pagine/about/about';
 import { Contact } from './pagine/contact/contact';
 import { Service } from './pagine/service/service';
 import { Products } from './pagine/products/products';
-import { Splash } from './splash/splash';
 
 export const routes: Routes = [
-  { path: '', component: Splash },
-  { path: 'splash', component: Splash },
+  { path: '', component: Home },
   { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: 'service', component: Service },
